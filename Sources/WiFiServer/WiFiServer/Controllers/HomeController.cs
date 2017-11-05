2dd0d453bb4727c2c018eb38f4ec4090954ef4f9
patrykvan <patrykvan@gmail.com>
2017-11-05 17:35:01 +0100

    Add template WiFi server.

---
+++ Sources/WiFiServer/WiFiServer/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace WiFiServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            ViewBag.Title = "WiFi Tracker";
+            
+            return "Hello";
+        }
+    }
+}
---
+++ Sources/WiFiServer/WiFiServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WiFiServer.WebApiApplication" Language="C#" %>
